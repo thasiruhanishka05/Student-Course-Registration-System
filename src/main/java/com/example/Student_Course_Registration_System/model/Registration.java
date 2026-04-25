e03a84fb8c980139032df7fc4326867e3bd1fd40
IT25101575 <it25101575@my.sliit.lk>
2026-04-25 20:28:41 +0530

    Update Registration.java
    add student regi:

--- src/main/java/com/example/Student_Course_Registration_System/model/Registration.java
+++ src/main/java/com/example/Student_Course_Registration_System/model/Registration.java
@@ -1,4 +1,11 @@
 package com.example.Student_Course_Registration_System.model;
 
 public class Registration {
+    private int stdid;
+    private String stdname;
+
+    public Registration(int stdid,String stdname){
+        this.stdid=stdid;
+        this.stdname=stdname;
+    }
 }
